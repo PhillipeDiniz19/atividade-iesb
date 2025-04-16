1769fb20a3523287539e849b399176484e0c91b9
Phillipe Diniz Cardoso <phillipe.cardoso@corregedoria.df>
2025-04-16 08:33:49 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Mensagem enviada! Entraremos em contato em breve.");
+    this.reset();
+});
